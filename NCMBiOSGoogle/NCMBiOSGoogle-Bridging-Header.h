2d948d445ab6b3b66f65f382da3bab3fe23030e7
Naoki Tsutsui <naokitsutsui@me.com>
2015-07-15 23:32:10 +0900

    close #1 ブリッジヘッダーの追加

---
+++ NCMBiOSGoogle/NCMBiOSGoogle-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <NCMB/NCMB.h>
+#import <NCMB/NCMBObject+Subclass.h>
+
+#import <Google/SignIn.h>
