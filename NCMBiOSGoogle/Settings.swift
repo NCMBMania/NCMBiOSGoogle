eb48f5cc08de245f329437eb55e44d77c81b9105
Naoki Tsutsui <naokitsutsui@me.com>
2015-07-15 23:55:03 +0900

    close #2

---
+++ NCMBiOSGoogle/Settings.swift
@@ -0,0 +1,16 @@
+//
+//  Settings.swift
+//  NCMBiOSGoogle
+//
+//  Created by naokits on 7/15/15.
+//  Copyright (c) 2015 Naoki Tsutsui. All rights reserved.
+//
+
+// ------------------------------------------------------------------------
+// MARK: - NCMBiOSのAPIキー
+// ------------------------------------------------------------------------
+
+/// アプリケーションキー
+let kNCMBiOSApplicationKey = "YOUR_APPLICATION_KEY"
+/// クライアントキー
+let kNCMBiOSClientKey = "YOUR_CLIENT_KEY"
